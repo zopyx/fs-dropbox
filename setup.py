f16543c3f5a8b965265a7bda053fb4d4a4071ad9
Sean C. Farley <sean-dev@farley.org>
2014-03-27 14:21:09 -0400

    Minor cleanup of setup script.
    Use the standard path (/usr/bin) for env.
    
    Capitalize PyFilesystem as in its documentation.
    
    PEP-8 fixes.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import os
 from distutils.core import setup
@@ -11,27 +11,27 @@ readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 long_description = file(readme).read()
 
 setup(
-    name = name,
-    version = versrel,
-    description = 'A pyFilesystem backend for the Dropbox API.',
-    long_description = long_description,
-    requires = [
+    name=name,
+    version=versrel,
+    description='A PyFilesystem backend for the Dropbox API.',
+    long_description=long_description,
+    requires=[
         'fs',
         'dropbox',
     ],
-    author = 'Ben Timby',
-    author_email = 'btimby@gmail.com',
-    maintainer = 'Ben Timby',
-    maintainer_email = 'btimby@gmail.com',
-    url = 'http://github.com/btimby/fs-dropbox/',
-    license = 'GPLv3',
+    author='Ben Timby',
+    author_email='btimby@gmail.com',
+    maintainer='Ben Timby',
+    maintainer_email='btimby@gmail.com',
+    url='http://github.com/btimby/fs-dropbox/',
+    license='GPLv3',
     py_modules=['dropboxfs'],
     package_data={'': ['README.rst']},
-    classifiers = (
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Topic :: Software Development :: Libraries :: Python Modules',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ),
 )
